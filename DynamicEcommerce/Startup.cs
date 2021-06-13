7be82fbe43ba1d0ff6492c71e9df4731345b2e5f
Brody's PC <hollinsbrody@gmail.com>
2021-06-13 22:13:07 +0200

    added images for our sample products

--- DynamicEcommerce/Startup.cs
+++ DynamicEcommerce/Startup.cs
@@ -55,7 +55,7 @@ namespace DynamicEcommerce
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }
