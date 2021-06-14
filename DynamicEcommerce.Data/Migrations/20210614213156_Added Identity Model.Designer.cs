2b70cab3dde59bec61326f6d86e34c051e8b72aa
Brody's PC <hollinsbrody@gmail.com>
2021-06-14 23:33:40 +0200

    added identity model to db

--- DynamicEcommerce.Data/Migrations/20210614213156_Added Identity Model.Designer.cs
+++ DynamicEcommerce.Data/Migrations/20210614213156_Added Identity Model.Designer.cs
@@ -4,14 +4,16 @@ using DynamicEcommerce.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DynamicEcommerce.Data.Migrations
 {
     [DbContext(typeof(DynamicEcommerceDbContext))]
-    partial class DynamicEcommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210614213156_Added Identity Model")]
+    partial class AddedIdentityModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DynamicEcommerce.Data/Models/ApplicationUser.cs
+++ DynamicEcommerce.Data/Models/ApplicationUser.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DynamicEcommerce.Data.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
-        public int Id { get; set; }
     }
 }
