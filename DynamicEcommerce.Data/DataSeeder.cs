90f918bf07e93e27e85c3493f085910cc0b97228
Brody's PC <hollinsbrody@gmail.com>
2021-06-15 22:29:19 +0200

    final changes before submittions

--- DynamicEcommerce.Data/DataSeeder.cs
+++ DynamicEcommerce.Data/DataSeeder.cs
@@ -21,7 +21,7 @@ namespace DynamicEcommerce.Data
         {
             var passwordHasher = new PasswordHasher<ApplicationUser>();
             
-            var user = new ApplicationUser
+            var adminUser = new ApplicationUser
             {
                 UserName = "admin@dynamic.com",
                 NormalizedUserName = "ADMIN@DYNAMIC.COM",
@@ -32,25 +32,114 @@ namespace DynamicEcommerce.Data
                 SecurityStamp = Guid.NewGuid().ToString()
             };
 
-            var passwordHashed = passwordHasher.HashPassword(user, "Dynam1c1337*");
-            user.PasswordHash = passwordHashed;
+            var employee = new ApplicationUser
+            {
+                UserName = "john@dynamic.com",
+                NormalizedUserName = "JOHN@DYNAMIC.COM",
+                Email = "john@dynamic.com",
+                NormalizedEmail = "JOHN@DYNAMIC.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var customer1 = new ApplicationUser
+            {
+                UserName = "josh@gmail.com",
+                NormalizedUserName = "JOSH@GMAIL.COM",
+                Email = "josh@gmail.com",
+                NormalizedEmail = "JOSH@GMAIL.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var customer2 = new ApplicationUser
+            {
+                UserName = "brody@gmail.com",
+                NormalizedUserName = "BRODY@GMAIL.COM",
+                Email = "brody@gmail.com",
+                NormalizedEmail = "BRODY@GMAIL.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var customer3 = new ApplicationUser
+            {
+                UserName = "bob@gmail.com",
+                NormalizedUserName = "BOB@GMAIL.COM",
+                Email = "bob@gmail.com",
+                NormalizedEmail = "BOB@GMAIL.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var passwordHashed = passwordHasher.HashPassword(adminUser, "Dynam1c1337*");
+            var sampleUserHashed = passwordHasher.HashPassword(adminUser, "Password1*");
+
+            adminUser.PasswordHash = passwordHashed;
+            employee.PasswordHash = sampleUserHashed;
+            customer1.PasswordHash = sampleUserHashed;
+            customer2.PasswordHash = sampleUserHashed;
+            customer3.PasswordHash = sampleUserHashed;
 
             var roleStore = new RoleStore<IdentityRole>(context);
             var userStore = new UserStore<ApplicationUser>(context);
 
             var hasAdminRole = context.Roles.Any(roles => roles.Name == "Admin");
+            var hasEmployeeRole = context.Roles.Any(roles => roles.Name == "Employee");
+            var hasCustomerRole = context.Roles.Any(roles => roles.Name == "Customer");
 
             if (!hasAdminRole)
             {
                 await roleStore.
                     CreateAsync(new IdentityRole {Name="Admin", NormalizedName="admin"});
             }
+            if (!hasEmployeeRole)
+            {
+                await roleStore.
+                    CreateAsync(new IdentityRole { Name = "Employee", NormalizedName = "employee" });
+            }
+            if (!hasCustomerRole)
+            {
+                await roleStore.
+                    CreateAsync(new IdentityRole { Name = "Customer", NormalizedName = "customer" });
+            }
 
-            var hasAdminUser = context.Users.Any(x => x.UserName == user.UserName);
+
+            var hasAdminUser = context.Users.Any(x => x.UserName == adminUser.UserName);
+            var hasEmployee = context.Users.Any(x => x.UserName == employee.UserName);
+            var hasCustomer1 = context.Users.Any(x => x.UserName == customer1.UserName);
+            var hasCustomer2 = context.Users.Any(x => x.UserName == customer2.UserName);
+            var hasCustomer3 = context.Users.Any(x => x.UserName == customer3.UserName);
+
             if (!hasAdminUser)
             {
-                await userStore.CreateAsync(user);
-                await userStore.AddToRoleAsync(user,  "Admin");
+                await userStore.CreateAsync(adminUser);
+                await userStore.AddToRoleAsync(adminUser,  "Admin");
+            }
+            if (!hasEmployee)
+            {
+                await userStore.CreateAsync(employee);
+                await userStore.AddToRoleAsync(employee, "Customer");
+            }
+
+            if (!hasCustomer1)
+            {
+                await userStore.CreateAsync(customer1);
+                await userStore.AddToRoleAsync(customer1, "Customer");
+            }
+
+            if (!hasCustomer2)
+            {
+                await userStore.CreateAsync(customer2);
+                await userStore.AddToRoleAsync(customer2, "Customer");
+            }
+
+            if (!hasCustomer3)
+            {
+                await userStore.CreateAsync(customer3);
+                await userStore.AddToRoleAsync(customer3, "Customer");
             }
 
             await context.SaveChangesAsync();
